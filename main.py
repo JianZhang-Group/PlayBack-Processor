b830d4fd01fe51c72d11f5edce15013e415f348d
D0831 <364044341@qq.com>
2025-07-31 20:39:49 +0800

    Fix: DPI Adapt

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import Qt, QCoreApplication
 from gui.main_window import MainWindow
 import sys
 
 def main():
+    # 启用高DPI缩放
+    QCoreApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
+    QCoreApplication.setAttribute(Qt.AA_UseHighDpiPixmaps)
+    
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
